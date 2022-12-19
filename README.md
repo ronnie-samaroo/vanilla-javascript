d1695a24389fdf02f22a98255badb51e88b5dede
jose-cipriano <webdev10250907@gmail.com>
2022-12-18 22:35:52 -0300

    project setup

---
+++ README.md
@@ -0,0 +1,46 @@
+  <h1 align="center">
+  <img src="src/assets/images/MetaPals.png" width="100px" height="70px"/>
+  Catch a Mole
+  <img src="src/assets/images/mole.png" width="100px" height="50px"/>
+  </h1>
+
+### Testable Demo
+
+You can play with mole here:
+TODO: deploy when it's completed...
+
+### Overview
+
+This is a hello all game built with HTML, CSS, Vanilla JS.
+
+## Setup
+
+To install the project run the command:
+
+```
+yarn install
+```
+
+### start app
+
+To run the project run the command:
+
+```
+yarn start
+```
+
+### build app
+
+To build the app run the command:
+
+```
+yarn build
+```
+
+### deploy app
+
+To deploy the project run the command:
+
+```
+yarn deploy
+```
---
+++ webpack.config.js
@@ -0,0 +1,59 @@
+const path = require('path');
+const rules = require('require.all')('./tasks/rules');
+const plugins = require('require.all')('./tasks/plugins');
+
+module.exports = (env, options) => {
+  const modes = {
+    development: 'development',
+    production: 'production'
+  };
+
+  const environment = modes[options.mode] || modes.development;
+
+  rules((name, rule) => rule(environment));
+  plugins((name, rule) => rule(environment));
+
+  return {
+    mode: environment,
+    entry: {
+      app: path.resolve(__dirname, 'src/index.js')
+    },
+    output: {
+      filename: '[name].js'
+    },
+    module: {
+      rules: [...rules.files, rules.scripts, rules.styles]
+    },
+    plugins: [plugins.html, plugins.images, plugins.extractStyles, plugins.purgeStyles],
+    devServer: {
+      open: true,
+      port: 4000,
+      https: false,
+      hot: true,
+      historyApiFallback: true,
+      watchOptions: {
+        poll: true
+      }
+      // proxy: { '/api': 'http://localhost:3000' }
+    },
+    optimization: {
+      minimizer: [plugins.uglify],
+      splitChunks: {
+        cacheGroups: {
+          vendor: {
+            chunks: 'all',
+            test: path.resolve(__dirname, 'node_modules'),
+            name: 'vendor',
+            enforce: true
+          }
+        }
+      }
+    },
+    resolve: {
+      alias: {
+        assets: path.resolve(__dirname, 'src/assets'),
+        '~': path.resolve(__dirname, 'src/app')
+      }
+    }
+  };
+};
